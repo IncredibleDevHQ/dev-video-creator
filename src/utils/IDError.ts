0c19734ab650cb33f961d48e6a206b7355ef4672
Aakash Sharma <9267aakashsharma@gmail.com>
2021-07-28 02:09:59 +0530

    ✨ Add error-boundary component

---
+++ src/utils/IDError.ts
@@ -0,0 +1,11 @@
+class IDError extends Error {
+  title?: string
+  constructor(message: string, title?: string) {
+    super()
+
+    this.title = title
+    this.message = message
+  }
+}
+
+export default IDError
