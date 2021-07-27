456b468830762907b51877f454bdcf24bb005fbb
Aakash Sharma <9267aakashsharma@gmail.com>
2021-07-27 18:34:38 +0530

    🔧 Add Tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        inherit: 'inherit',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import svgr from 'vite-plugin-svgr'
+import vitePluginHtmlEnv from 'vite-plugin-html-env'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  build: {
+    outDir: 'build',
+    sourcemap: true,
+  },
+  plugins: [reactRefresh(), svgr(), vitePluginHtmlEnv()],
+  resolve: {
+    alias: {
+      'tailwind.config.js': path.resolve(__dirname, 'tailwind.config.js'),
+    },
+  },
+  optimizeDeps: {
+    include: ['tailwind.config.js'],
+  },
 })
