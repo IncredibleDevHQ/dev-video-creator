aecf1faafa51d573505334a206ada576d05872dc
Aakash Sharma <9267aakashsharma@gmail.com>
2021-07-31 20:54:23 +0530

    🍱 Add Assets and icons

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+import LOGO from './assets/logo.svg'
+import GOOGLE from './assets/google.svg'
+import GITHUB from './assets/github.gif'
+import LOGIN_USER_ANIMATION from './assets/SingIn.json'
+
+export const ASSETS = {
+  ICONS: {
+    LOGO,
+    GOOGLE,
+    GITHUB,
+  },
+  ANIMATION: {
+    LOGIN_USER_ANIMATION,
+  },
+}
