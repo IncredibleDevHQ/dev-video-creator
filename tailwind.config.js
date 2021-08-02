e7d0fb3b47ed2d441138720d1e423f7c9a3a5bb1
Aakash Sharma <9267aakashsharma@gmail.com>
2021-08-02 13:04:17 +0530

    🔧 Add brand colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     extend: {
       colors: {
         inherit: 'inherit',
+        brand: '#5156EA',
+        'brand-alt': '#51A3EA',
+        'brand-background': '#FFFFFF',
+        'brand-background-alt': '#FBFCFF',
+        'brand-success': '#42E735',
+        'brand-warning': '#F2EC24',
+        'brand-error': '#F22424',
       },
     },
   },
