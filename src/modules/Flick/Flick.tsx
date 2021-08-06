d54584e13c65366917f46c4abda2679545c62ca4
Aakash Sharma <9267aakashsharma@gmail.com>
2021-08-06 18:29:31 +0530

    ✨ Add Flick

--- src/modules/Flick/Flick.tsx
+++ src/modules/Flick/Flick.tsx
@@ -1,18 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
-import { ScreenState } from '../../components'
-import { useGetFlickByIdQuery } from '../../generated/graphql'
+import {
+  AddFragmentModal,
+  FragmentActivity,
+  FragmentConfiguration,
+  FragmentsSidebar,
+  Participants,
+} from './components'
 import { currentFlickStore } from '../../stores/flick.store'
+import { EmptyState, ScreenState, Tab, TabBar } from '../../components'
+import { useGetFlickByIdQuery } from '../../generated/graphql'
+
+const tabs: Tab[] = [
+  {
+    name: 'Activity',
+    value: 'Activity',
+  },
+  {
+    name: 'Configuration',
+    value: 'Configuration',
+  },
+]
 
 const Flick = () => {
   const { id } = useParams<{ id: string }>()
-  const { data, error, loading } = useGetFlickByIdQuery({ variables: { id } })
+  const { data, error, loading, refetch } = useGetFlickByIdQuery({
+    variables: { id },
+  })
   const [flick, setFlick] = useRecoilState(currentFlickStore)
+  const [currentTab, setCurrentTab] = useState<Tab>(tabs[0])
+  const [isParticipants, setParticipants] = useState(true)
+  const [isAddFragmentModal, setAddFragmentModal] = useState(false)
+  const [activeFragmentId, setActiveFragmentId] = useState<string>(
+    flick?.fragments[0].id
+  )
 
   useEffect(() => {
     if (!data?.Flick_by_pk) return
     setFlick(data.Flick_by_pk)
+    setActiveFragmentId(data.Flick_by_pk.fragments[0].id)
   }, [data])
 
   if (loading) return <ScreenState title="Just a jiffy" loading />
@@ -22,7 +49,59 @@ const Flick = () => {
       <ScreenState title="Something went wrong!!" subtitle={error.message} />
     )
 
-  return <section>{flick?.name}</section>
+  if (!flick)
+    return (
+      <ScreenState
+        title="Something went wrong!!"
+        subtitle="We couldn't find the details of this flick"
+      />
+    )
+
+  return (
+    <section className="flex flex-row relative">
+      <FragmentsSidebar
+        fragments={flick.fragments}
+        activeFragmentId={activeFragmentId}
+        setActiveFragmentId={setActiveFragmentId}
+        setAddFragmentModal={setAddFragmentModal}
+      />
+      <div className="flex-1 p-4">
+        {activeFragmentId ? (
+          <div>
+            <TabBar
+              tabs={tabs}
+              current={currentTab}
+              onTabChange={setCurrentTab}
+            />
+            {currentTab.value === 'Configuration' && (
+              <FragmentConfiguration
+                fragment={flick.fragments.find(
+                  (fragment) => fragment.id === activeFragmentId
+                )}
+              />
+            )}
+            {currentTab.value === 'Activity' && <FragmentActivity />}
+          </div>
+        ) : (
+          <EmptyState text="No Fragment is selected" width={400} />
+        )}
+      </div>
+      <Participants
+        isParticipants={isParticipants}
+        setParticipants={setParticipants}
+        participants={flick.participants}
+      />
+      <AddFragmentModal
+        open={isAddFragmentModal}
+        flickId={flick.id}
+        totalFragments={flick.fragments.length}
+        handleClose={(refresh) => {
+          if (refresh) refetch()
+          setAddFragmentModal(false)
+        }}
+      />
+    </section>
+  )
 }
 
 export default Flick
