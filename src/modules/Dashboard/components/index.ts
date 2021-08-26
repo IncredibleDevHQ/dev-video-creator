6bdb976a1666ff1fcf3db162410fb785f58d7e55
Ishan Chhabra <me@ishanchhabra.com>
2021-08-26 14:47:04 +0530

    🎨 Segregate components

--- src/modules/Dashboard/components/index.ts
+++ src/modules/Dashboard/components/index.ts
@@ -1,4 +1,6 @@
 import NewFlickBanner from './NewFlickBanner'
+import TableView from './TableView'
+import GridView from './GridView'
 import UpcomingFlickBanner from './UpcomingFlickBanner'
 
-export { NewFlickBanner, UpcomingFlickBanner }
+export { NewFlickBanner, UpcomingFlickBanner, TableView, GridView }
