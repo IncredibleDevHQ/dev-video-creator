e1fd15be84b227ee59a330d913c5b05da4f53c8e
Suvij Surya <suvijsurya76@gmail.com>
2021-09-07 17:20:29 +0530

    💄 Add new transitions

--- src/modules/Studio/effects/FragmentTransitions.tsx
+++ src/modules/Studio/effects/FragmentTransitions.tsx
@@ -1,3 +1,4 @@
+import Konva from 'konva'
 import React from 'react'
 import { Group, Circle, Text, Rect } from 'react-konva'
 import { CONFIG } from '../components/Concourse'
@@ -119,22 +120,55 @@ export const CircleCenterGrow = ({
   )
 }
 
-export const SquareCenterTransition = ({
+export const RectCenterGrow = ({
   performFinishAction,
 }: {
   performFinishAction?: () => void
 }) => {
   return (
-    <Circle
-      x={CONFIG.width / 2}
+    <Rect
+      x={0}
       y={CONFIG.height / 2}
-      radius={600}
       fill="#16A34A"
+      width={CONFIG.width}
+      height={1}
       ref={(ref) =>
         ref?.to({
-          scaleX: 0,
-          scaleY: 0,
-          duration: 0.15,
+          height: CONFIG.height,
+          y: 0,
+          duration: 0.2,
+          easing: Konva.Easings.EaseOut,
+          onFinish: () => {
+            if (!performFinishAction) return
+            setTimeout(() => {
+              performFinishAction()
+            }, 200)
+          },
+        })
+      }
+    />
+  )
+}
+
+export const RectCenterShrink = ({
+  performFinishAction,
+}: {
+  performFinishAction?: () => void
+}) => {
+  return (
+    <Rect
+      x={0}
+      y={0}
+      fill="#16A34A"
+      width={CONFIG.width}
+      height={CONFIG.height}
+      opacity={1}
+      ref={(ref) =>
+        ref?.to({
+          height: 240,
+          y: CONFIG.height / 2 - 120,
+          duration: 0.2,
+          easing: Konva.Easings.EaseOut,
           onFinish: () => {
             ref?.to({ opacity: 0 })
             if (!performFinishAction) return
