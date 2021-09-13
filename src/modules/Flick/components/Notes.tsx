ffef3a7d573dca9ef38f92ad92d59b631ad5b1c6
hemalathank-27 <hemaalatha@incredible.dev>
2021-09-13 23:08:45 +0530

    ✨ Add front end for notes of flick .

---
+++ src/modules/Flick/components/Notes.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Heading, Text } from '../../../components'
+import { useGetFlickNotesQuery } from '../../../generated/graphql'
+
+const Notes = ({ flickId }: { flickId: string }) => {
+  const { data, loading } = useGetFlickNotesQuery({
+    variables: {
+      flickId, // value for 'flickId'
+    },
+  })
+  return loading ? (
+    <Text className="text-center text-xs"> loading...</Text>
+  ) : (
+    <div>
+      {data &&
+        data.Note.length > 0 &&
+        data.Note.map((note) => (
+          <div className="mb-3">
+            <Heading fontSize="medium" className="text-xl">
+              {note.sourceUrl}
+            </Heading>
+            <Text className="h-20 text-sm overflow-hidden overflow-ellipsis">
+              {note.note}
+            </Text>
+          </div>
+        ))}
+      {data && data.Note.length === 0 && (
+        <div className="flex flex-col justify-center">
+          <Text className="text-center text-xs">
+            Oops you haven't Added any notes yet!
+          </Text>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Notes
