de9849d5626b98cb4ee344b049f367b3ccf408de
Shreya Tabjul <stabjul@gmail.com>
2021-09-17 16:38:20 +0530

    ✨ Add splash template for cockroach

--- src/modules/Studio/effects/SplashTwelve.tsx
+++ src/modules/Studio/effects/SplashTwelve.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Circle, Rect, Text, Line } from 'react-konva'
+import { Circle, Rect, Text, Line, Group, Image } from 'react-konva'
 import { useRecoilValue } from 'recoil'
+import useImage from 'use-image'
 import { useParams } from 'react-router-dom'
 import Concourse, { CONFIG } from '../components/Concourse'
 import { StudioProviderProps, studioStore } from '../stores'
@@ -8,6 +9,7 @@ import useSplash, { Coordinates } from '../hooks/use-splash'
 import { User, userState } from '../../../stores/user.store'
 import { useGetFragmentByIdQuery } from '../../../generated/graphql'
 import { EmptyState } from '../../../components'
+import config from '../../../config'
 
 const titleEnum = 'title'
 const subTitleEnum = 'subtitle'
@@ -18,6 +20,10 @@ const SplashTwelve = () => {
   const [configuration, setConfiguration] =
     useState<{ title: any; subTitle: any }>()
   const params: { fragmentId: string } = useParams()
+  const [cockroachLogo] = useImage(
+    `${config.storage.baseUrl}cockroachLogoSVG.svg`,
+    'anonymous'
+  )
 
   const { data } = useGetFragmentByIdQuery({
     variables: { id: params.fragmentId, sub: sub as string },
@@ -68,16 +74,24 @@ const SplashTwelve = () => {
     }
   }, [state, configuration])
 
-  const gradientStroke = () => {
+  const gradientStrokeOne = () => {
     const canvas = document.createElement('canvas')
     const ctx = canvas.getContext('2d')
 
-    const gradient = ctx?.createLinearGradient(0, 50, 100, 50)
-    gradient?.addColorStop(0.1, '#21E8EE')
-    gradient?.addColorStop(0.25, '#4E7BFA')
-    gradient?.addColorStop(0.5, '#5B5AFD')
-    gradient?.addColorStop(0.75, '#6249FF')
-    gradient?.addColorStop(0.9, '#6347FF')
+    const gradient = ctx?.createLinearGradient(0, 100, 100, 0)
+    gradient?.addColorStop(0, 'rgba(33, 232, 238, 1)')
+    gradient?.addColorStop(1, 'rgba(99, 71, 255, 1)')
+
+    return gradient
+  }
+
+  const gradientStrokeTwo = () => {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+
+    const gradient = ctx?.createLinearGradient(0, 100, 100, 0)
+    gradient?.addColorStop(0, 'rgba(80, 118, 248, 0)')
+    gradient?.addColorStop(1, 'rgba(79, 118, 248, 1)')
 
     return gradient
   }
@@ -97,34 +111,90 @@ const SplashTwelve = () => {
       ...layerChildren,
       <Circle
         key="FirstCircle"
-        x={345}
+        x={200}
         y={175}
         radius={78}
-        stroke={gradientStroke()}
+        stroke={gradientStrokeOne()}
         strokeWidth={39}
+        scaleX={3}
+        scaleY={3}
         opacity={0}
         ref={(ref) => {
           ref?.to({
-            duration: 1,
+            duration: 2,
             opacity: 1,
           })
         }}
       />,
-      //   <Circle
-      //     key="SecondCircle"
-      //     x={458}
-      //     y={208}
-      //     radius={78}
-      //     stroke={gradientStroke()}
-      //     strokeWidth={39}
-      //     opacity={0}
-      //     ref={(ref) => {
-      //       ref?.to({
-      //         duration: 1,
-      //         opacity: 1,
-      //       })
-      //     }}
-      //   />,
+      <Circle
+        key="SecondCircle"
+        x={670}
+        y={250}
+        radius={78}
+        stroke={gradientStrokeTwo()}
+        strokeWidth={39}
+        scaleX={3}
+        scaleY={3}
+        opacity={0}
+        ref={(ref) => {
+          ref?.to({
+            duration: 2,
+            opacity: 1,
+          })
+        }}
+      />,
+      <Group x={114} y={161} width={732} height={184} key="group1">
+        <Text
+          fontSize={104}
+          fill="#FFFFFF"
+          fontFamily="Poppins"
+          fontStyle="normal 700"
+          text="CockroachDB"
+          align="center"
+          opacity={0}
+          ref={(ref) => {
+            setTimeout(() => {
+              ref?.to({
+                duration: 0.5,
+                opacity: 1,
+              })
+            }, 2000)
+          }}
+        />
+        <Text
+          fontSize={24}
+          fill="#FFFFFF"
+          fontFamily="Poppins"
+          text="The most highly evolved database on planet"
+          fontStyle="normal 400"
+          align="center"
+          y={126}
+          x={65}
+          opacity={0}
+          ref={(ref) => {
+            setTimeout(() => {
+              ref?.to({
+                duration: 0.5,
+                opacity: 1,
+              })
+            }, 2000)
+          }}
+        />
+      </Group>,
+      <Group x={697} y={476} width={220} height={32} key="group2">
+        <Image
+          image={cockroachLogo}
+          opacity={0}
+          ref={(ref) => {
+            setTimeout(() => {
+              ref?.to({
+                duration: 0.5,
+                opacity: 1,
+              })
+            }, 3000)
+          }}
+        />
+      </Group>,
     ])
   }
   if (!configuration)
