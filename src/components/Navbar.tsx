768d6c4ef90d2823a55e006d104172b0a950a918
blackzphoenix <zphoenix@disroot.org>
2021-09-23 15:19:40 +0530

    ⚡️ Logo Alignment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,7 @@ import { User, userState } from '../stores/user.store'
 import logo from '../assets/logo.svg'
 import { Auth, authState } from '../stores/auth.store'
 import Button from './Button'
+import { ASSETS } from '../constants'
 
 const LinkItem = ({ children, to }: { children: string; to: string }) => (
   <NavLink activeClassName="text-brand" className={cx('font-semibold')} to={to}>
@@ -22,7 +23,7 @@ const AuthenticatedRightCol = () => {
 
   return (
     <div className="flex items-center">
-      <div className="relative mr-4">
+      {/* <div className="relative mr-4">
         <span
           style={{ top: 0, right: 2 }}
           className="block bg-red-600  absolute w-1.5 h-1.5 rounded-full"
@@ -30,7 +31,7 @@ const AuthenticatedRightCol = () => {
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-600 duration-500 opacity-75" />
         </span>
         <FiBell />
-      </div>
+      </div> */}
 
       <div
         role="button"
@@ -45,18 +46,19 @@ const AuthenticatedRightCol = () => {
         <div className="w-8 h-8 relative">
           <span
             style={{ zIndex: 0 }}
-            className="top-0 left-0 w-8 h-8 rounded-full absolute animate-spin-slow bg-gradient-to-r from-brand to-brand-alt"
+            // className="top-0 left-0 w-8 h-8 rounded-full absolute animate-spin-slow bg-gradient-to-r from-brand to-brand-alt"
+            className="top-0 left-0 w-8 h-8 rounded-full absolute animate-spin-slow "
           />
-          <div className="z-10 absolute top-1/2 w-6 h-6 transform -translate-x-1/2 -translate-y-1/2 left-1/2">
+          <div className="z-10  w-8 h-8  absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2 left-1/2">
             {picture ? (
               <img
                 src={picture}
                 alt={displayName || 'user'}
-                className="w-6 h-6 rounded-full bg-gray-100"
+                className="w-8 h-8 rounded-full bg-gray-100"
               />
             ) : (
               <Gravatar
-                className="w-6 h-6 rounded-full bg-gray-100"
+                className="w-8 h-8 rounded-full bg-gray-100"
                 email={email as string}
               />
             )}
@@ -72,16 +74,16 @@ const Navbar = ({ hideNav }: { hideNav?: boolean }) => {
   const { isAuthenticated } = (useRecoilValue(authState) as Auth) || {}
 
   return (
-    <nav className="flex flex-row items-center m-4 px-4 py-2 bg-gray-100 rounded-lg justify-between">
-      <img className="w-10" src={logo} alt="Logo" />
+    <nav className="flex flex-row items-center px-4 py-2 justify-between border-b-2">
+      <img src={ASSETS.ICONS.IncredibleLogo} alt="" className="w-28" />
 
       {hideNav ? (
         <span />
       ) : (
         <ul className="grid grid-flow-col md:gap-x-6 gap-x-4">
-          <LinkItem to="/dashboard">Dashboard</LinkItem>
+          {/* <LinkItem to="/dashboard">Dashboard</LinkItem>
           <LinkItem to="/organisations">Organisations</LinkItem>
-          <LinkItem to="/circle">Circle</LinkItem>
+          <LinkItem to="/circle">Circle</LinkItem> */}
         </ul>
       )}
 
