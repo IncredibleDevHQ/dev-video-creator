d0c056af2bff6e34f77b11770faf5f57f7be9db9
Shreya Tabjul <stabjul@gmail.com>
2021-09-28 14:25:02 +0530

    🐛 Display only undeleted flicks in add flicks  modal

--- src/modules/Flick/components/FlickCard.tsx
+++ src/modules/Flick/components/FlickCard.tsx
@@ -15,18 +15,22 @@ const FlickCard = ({
   const history = useHistory()
 
   return (
-    <div
-      role="link"
-      onKeyUp={() => {}}
-      tabIndex={flick.id}
-      onClick={onClick || (() => history.push(`/flick/${flick.id}`))}
-      className={`border-brand border-2 rounded-md cursor-pointer w-48 h-28 hover:border-brand ${
-        selected && 'border-brand border-l-4'
-      }`}
-    >
-      <Heading className="text-sm w-44 truncate overflow-ellipsis capitalize">
-        {flick.name}
-      </Heading>
+    <div>
+      {!flick.deletedAt && (
+        <div
+          role="link"
+          onKeyUp={() => {}}
+          tabIndex={flick.id}
+          onClick={onClick || (() => history.push(`/flick/${flick.id}`))}
+          className={`border-brand border-2 rounded-md cursor-pointer w-48 h-28 hover:border-brand ${
+            selected && 'border-brand border-l-4'
+          }`}
+        >
+          <Heading className="text-sm w-44 truncate overflow-ellipsis capitalize">
+            {flick.name}
+          </Heading>
+        </div>
+      )}
     </div>
   )
 }
