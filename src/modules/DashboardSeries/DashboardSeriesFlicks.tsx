5d0615bc51908522189ef7e6208e8f27c4732798
Shreya Tabjul <stabjul@gmail.com>
2021-10-04 13:14:39 +0530

    ✨ Multiple options inside ellipses, flick count updated in dashboard

--- src/modules/DashboardSeries/DashboardSeriesFlicks.tsx
+++ src/modules/DashboardSeries/DashboardSeriesFlicks.tsx
@@ -2,7 +2,11 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { EmptyState, Heading, ScreenState, Text } from '../../components'
 import { Icons } from '../../constants'
-import { useGetUserSeriesQuery } from '../../generated/graphql'
+import {
+  Flick_Status_Enum_Enum,
+  useGetUserSeriesQuery,
+} from '../../generated/graphql'
+import seriesModal from '../Series/OrganisationSeries/CreateSeriesModal'
 
 const DashboardSeriesFlicks = () => {
   const { data, error } = useGetUserSeriesQuery({
@@ -18,8 +22,8 @@ const DashboardSeriesFlicks = () => {
 
   useEffect(() => {
     console.log(
-      'log',
-      data?.Series.map((p) => p.Flick_Series[0])
+      'data',
+      data?.Series.map((p) => p.flickCount?.count)
     )
   }, [])
 
@@ -30,33 +34,22 @@ const DashboardSeriesFlicks = () => {
       )}
       <div className="gap-y-5 p-0 grid grid-cols-4 mr-20 justify-center mt-7 mb-20 rounded-md">
         {data &&
-          data.Series.length > 0 &&
-          data.Series.map((series) => (
-            <Link to={`/series/${series.id}`} key={series.id}>
+          data.Series.map((serie) => (
+            <Link to={`/series/${serie.id}`} key={serie.id}>
               <div
-                key={series.id}
+                key={serie.id}
                 className="bg-gray-50 hover:border-green-500 cursor-pointer w-60 h-36 rounded-md border-gray-300 border-2 items-center justify-center"
               >
                 <img src={Icons.seriesFolder} alt="I" className="ml-20 mt-10" />
                 <div className="bg-gray-300 h-5 w-14 p-1 justify-end ml-44 mb-20 mt-2 rounded-sm text-xs items-center">
-                  {series.Flick_Series.map(
-                    (p) => p.seriesFlickCount && !p.seriesFlickCount?.count
-                  ) ? (
-                    <Text> 0 flicks </Text>
-                  ) : (
-                    <div>
-                      {series.Flick_Series.map((f) =>
-                        f.seriesFlickCount?.count > 1
-                          ? `${f.seriesFlickCount?.count} Flicks`
-                          : `${f.seriesFlickCount?.count} Flick`
-                      )}
-                    </div>
-                  )}
+                  {!serie.flickCount
+                    ? `0 flicks`
+                    : `${serie.flickCount?.count} Flicks`}
                 </div>
               </div>
               <div className="w-full">
                 <Heading className="text-sm md:capitalize p-2 mt-0 font-semibold text-gray-800 w-40 truncate overflow-ellipsis">
-                  {series.name}
+                  {serie.name}
                 </Heading>
               </div>
             </Link>
