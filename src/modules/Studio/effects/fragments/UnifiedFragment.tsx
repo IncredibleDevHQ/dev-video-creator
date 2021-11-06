d91c56c504ed4740a9fa6fbe70bb96a0c9025785
Nithin Kumar B <nithin@incredible.dev>
2021-11-07 00:00:37 +0530

    ♻️ Remove comments

--- src/modules/Studio/effects/fragments/UnifiedFragment.tsx
+++ src/modules/Studio/effects/fragments/UnifiedFragment.tsx
@@ -54,10 +54,6 @@ const UnifiedFragment = ({
   // holds the user's display name
   const { displayName } = (useRecoilValue(userState) as User) || {}
 
-  // useEffect(() => {
-  //   console.log('layerref', layerRef.current)
-  // }, [layerRef])
-
   useEffect(() => {
     if (!config) return
     setDataConfig(config.dataConfig)
@@ -73,226 +69,6 @@ const UnifiedFragment = ({
       setDataConfig(config.dataConfig)
       setViewConfig(config.viewConfig)
     }
-    // setDataConfig([
-    //   {
-    //     id: 'ijRZdrawIO_9_yqpYS76g',
-    //     type: ConfigType.CODEJAM,
-    //     title: '',
-    //     value: {
-    //       code: 'print("Hellow")',
-    //       gistURL: '',
-    //       isAutomated: false,
-    //       language: 'python',
-    //       explanations: [],
-    //       colorCodes: [
-    //         {
-    //           color: '#D4D4D4',
-    //           lineNumber: 0,
-    //           content: 'W = tf.Variable(tf.ones([num_features, num_classes]), ',
-    //         },
-    //         { color: '#9CDCFE', lineNumber: 1, content: 'name' },
-    //         { color: '#D4D4D4', lineNumber: 1, content: '=' },
-    //         { color: '#CE9178', lineNumber: 1, content: '"weight"' },
-    //         { color: '#D4D4D4', lineNumber: 1, content: ')' },
-    //         { color: '#DCDCAA', lineNumber: 2, content: 'print' },
-    //         { color: '#D4D4D4', lineNumber: 2, content: '(W)' },
-    //         {
-    //           color: '#D4D4D4',
-    //           lineNumber: 3,
-    //           content: 'W = tf.Variable(tf.ones([num_features, num_classes]), ',
-    //         },
-    //         { color: '#9CDCFE', lineNumber: 3, content: 'name' },
-    //         { color: '#D4D4D4', lineNumber: 3, content: '=' },
-    //         { color: '#CE9178', lineNumber: 3, content: '"weight"' },
-    //         { color: '#D4D4D4', lineNumber: 3, content: ')' },
-    //         { color: '#DCDCAA', lineNumber: 4, content: 'print' },
-    //         { color: '#D4D4D4', lineNumber: 4, content: '(W)' },
-    //         {
-    //           color: '#D4D4D4',
-    //           lineNumber: 5,
-    //           content: 'b = tf.Variable(tf.zeros([num_classes]), ',
-    //         },
-    //         { color: '#9CDCFE', lineNumber: 5, content: 'name' },
-    //         { color: '#D4D4D4', lineNumber: 5, content: '=' },
-    //         { color: '#CE9178', lineNumber: 5, content: '"bias"' },
-    //         { color: '#D4D4D4', lineNumber: 5, content: ')' },
-    //         {
-    //           color: '#D4D4D4',
-    //           lineNumber: 6,
-    //           content: 'b = tf.Variable(tf.zeros([num_classes]), ',
-    //         },
-    //         { color: '#9CDCFE', lineNumber: 6, content: 'name' },
-    //         { color: '#D4D4D4', lineNumber: 6, content: '=' },
-    //         { color: '#CE9178', lineNumber: 6, content: '"bias"' },
-    //         { color: '#D4D4D4', lineNumber: 6, content: ')' },
-    //       ],
-    //     },
-    //     notes: ['Notes for above code block'],
-    //   },
-    //   {
-    //     id: 'aZHtrekf5WAF3FXMpHYWi',
-    //     type: ConfigType.TRIVIA,
-    //     title: 'Trivia/Slides Heading',
-    //     value: {
-    //       text: 'Question1',
-    //     },
-    //     notes: ['Notes for question1'],
-    //   },
-    //   {
-    //     id: '8Wi1JpgnD86jMJI1mIvoO',
-    //     type: ConfigType.TRIVIA,
-    //     title: 'Trivia/Slides Heading',
-    //     value: {
-    //       text: 'Question2-Image',
-    //       image: 'https://cdn.incredible.dev/idev-logo.svg',
-    //     },
-    //     notes: ['Notes for question2-image'],
-    //   },
-    //   {
-    //     id: 'ebwwlXN-IPcl6xyPJVSVy',
-    //     type: ConfigType.POINTS,
-    //     title: 'Points',
-    //     value: [
-    //       {
-    //         level: 0,
-    //         text: 'Point1',
-    //       },
-    //       {
-    //         level: 0,
-    //         text: 'Point2',
-    //       },
-    //       {
-    //         level: 0,
-    //         text: 'Point3',
-    //       },
-    //     ],
-    //     notes: ['Notes about points'],
-    //   },
-    //   {
-    //     id: 'g45uLwBxRkGP3w8Zm9V4b',
-    //     type: ConfigType.VIDEOJAM,
-    //     title: 'VideoJam',
-    //     value: {
-    //       videoURL:
-    //         'https://incredible-uploads-staging.s3.amazonaws.com/gmrY12qQHtlYH3JrqaEqp.webm',
-    //     },
-    //   },
-    // ])
-    // setViewConfig({
-    //   hasTitleSplash: true,
-    //   configs: [
-    //     {
-    //       id: '12345',
-    //       type: ConfigType.VIDEOJAM,
-    //       layoutNumber: 1,
-    //       background: {
-    //         type: 'color',
-    //         gradient: {
-    //           cssString:
-    //             'linear-gradient(90deg, #D397FA 0%, #D397FA 0.01%, #8364E8 100%)',
-    //           values: [0, '#D397FA', 0.0001, '#D397FA', 1, '#8364E8'],
-    //           startIndex: {
-    //             x: 0,
-    //             y: 269.99999999999994,
-    //           },
-    //           endIndex: {
-    //             x: 960,
-    //             y: 270.00000000000006,
-    //           },
-    //         },
-    //         image: '',
-    //       },
-    //     },
-    //     {
-    //       id: '123456',
-    //       type: ConfigType.CODEJAM,
-    //       layoutNumber: 1,
-    //       background: {
-    //         type: 'color',
-    //         gradient: {
-    //           cssString:
-    //             'linear-gradient(90deg, #D397FA 0%, #D397FA 0.01%, #8364E8 100%)',
-    //           values: [0, '#D397FA', 0.0001, '#D397FA', 1, '#8364E8'],
-    //           startIndex: {
-    //             x: 0,
-    //             y: 269.99999999999994,
-    //           },
-    //           endIndex: {
-    //             x: 960,
-    //             y: 270.00000000000006,
-    //           },
-    //         },
-    //         image: '',
-    //       },
-    //     },
-    //     {
-    //       id: '123456',
-    //       type: ConfigType.CODEJAM,
-    //       layoutNumber: 1,
-    //       background: {
-    //         type: 'color',
-    //         gradient: {
-    //           cssString:
-    //             'linear-gradient(90deg, #D397FA 0%, #D397FA 0.01%, #8364E8 100%)',
-    //           values: [0, '#D397FA', 0.0001, '#D397FA', 1, '#8364E8'],
-    //           startIndex: {
-    //             x: 0,
-    //             y: 269.99999999999994,
-    //           },
-    //           endIndex: {
-    //             x: 960,
-    //             y: 270.00000000000006,
-    //           },
-    //         },
-    //         image: '',
-    //       },
-    //     },
-    //     {
-    //       id: '123456',
-    //       type: ConfigType.CODEJAM,
-    //       layoutNumber: 1,
-    //       background: {
-    //         type: 'color',
-    //         gradient: {
-    //           cssString:
-    //             'linear-gradient(90deg, #D397FA 0%, #D397FA 0.01%, #8364E8 100%)',
-    //           values: [0, '#D397FA', 0.0001, '#D397FA', 1, '#8364E8'],
-    //           startIndex: {
-    //             x: 0,
-    //             y: 269.99999999999994,
-    //           },
-    //           endIndex: {
-    //             x: 960,
-    //             y: 270.00000000000006,
-    //           },
-    //         },
-    //         image: '',
-    //       },
-    //     },
-    //     {
-    //       id: '123456',
-    //       type: ConfigType.CODEJAM,
-    //       layoutNumber: 1,
-    //       background: {
-    //         type: 'color',
-    //         gradient: {
-    //           cssString:
-    //             'linear-gradient(90deg, #D397FA 0%, #D397FA 0.01%, #8364E8 100%)',
-    //           values: [0, '#D397FA', 0.0001, '#D397FA', 1, '#8364E8'],
-    //           startIndex: {
-    //             x: 0,
-    //             y: 269.99999999999994,
-    //           },
-    //           endIndex: {
-    //             x: 960,
-    //             y: 270.00000000000006,
-    //           },
-    //         },
-    //         image: '',
-    //       },
-    //     },
-    //   ],
-    // })
     settitleSplashData({
       enable: true,
       title: fragment.name as string,
@@ -301,9 +77,6 @@ const UnifiedFragment = ({
     // bgRectColor: ['#140D1F', '#6E1DDB'],
     //   stripRectColor: ['#FF5D01', '#B94301'],
     //   textColor: ['#E6E6E6', '#FFFFFF'],
-    updatePayload?.({
-      activeObjectIndex: 0,
-    })
   }, [fragment])
 
   useEffect(() => {
@@ -364,22 +137,6 @@ const UnifiedFragment = ({
     if (activeObjectIndex !== 0) settitleSplashData({ enable: false })
     else settitleSplashData({ enable: true, title: fragment?.name as string })
   }, [activeObjectIndex])
-
-  // useEffect(() => {
-  //   console.log('data', dataConfig)
-  // }, [dataConfig])
-
-  // useEffect(() => {
-  //   console.log('viewConfig', viewConfig)
-  // }, [viewConfig])
-
-  // useEffect(() => {
-  //   console.log('fragment', fragment)
-  // }, [fragment])
-
-  // useEffect(() => {
-  //   console.log('activeobjindx', activeObjectIndex)
-  // }, [activeObjectIndex])
 
   if (!dataConfig || !viewConfig || dataConfig.length === 0) return <></>
   return (
