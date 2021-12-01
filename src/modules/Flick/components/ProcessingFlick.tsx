5fcb961c6203cc904e8c70de4c48fc4d14418293
Aakash <9267aakashsharma@gmail.com>
2021-12-01 10:12:03 +0530

    🐛 Fix public URL on publish workflow

--- src/modules/Flick/components/ProcessingFlick.tsx
+++ src/modules/Flick/components/ProcessingFlick.tsx
@@ -72,12 +72,12 @@ const ProcessingFlick = ({
           </>
         )}
         <a
-          href={publicUrl + joinLink}
+          href={`${publicUrl}watch/${joinLink}`}
           target="_blank"
           rel="noreferrer noopener"
           className="flex my-4 border p-2 rounded-md items-center justify-between"
         >
-          {`${LINKS.WATCH}/watch/${joinLink}`}
+          {`${publicUrl}watch/${joinLink}`}
           <FiExternalLink size={24} className="mx-2" />
         </a>
       </div>
