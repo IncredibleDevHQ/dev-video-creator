1a89add8cbe488e936d22a27c7390836f359a089
Suvij Surya <suvijsurya76@gmail.com>
2021-12-03 09:53:19 +0530

    ✨ Add new intros to the splashes enum

--- src/modules/Flick/components/IntroConfig.tsx
+++ src/modules/Flick/components/IntroConfig.tsx
@@ -5,12 +5,16 @@ import { Text } from '../../../components'
 import { newFlickStore } from '../store/flickNew.store'
 
 enum Splashes {
+  GraphQL = '0',
+  Astro = '1',
+  TensorFlow = '2',
+  Default = '3',
+  Default2 = '4',
+  Default3 = '5',
+}
+
+enum Outros {
   Default = '0',
-  GraphQL = '1',
-  // OpenSauced = '2',
-  // Astro = '3',
-  // WTFJS = '4',
-  // Default2 = '5',
 }
 
 const IntroConfig = () => {
@@ -75,3 +79,64 @@ const IntroConfig = () => {
 }
 
 export default IntroConfig
+
+export const OutroConfig = () => {
+  const [{ flick, activeFragmentId }, setFlickStore] =
+    useRecoilState(newFlickStore)
+  const [outro, setOutro] = useState('0')
+
+  useEffect(() => {
+    setOutro(
+      `${
+        flick?.fragments.find((f) => f.id === activeFragmentId)?.configuration
+          ?.theme
+      }`
+    )
+  }, [
+    flick?.fragments.find((f) => f.id === activeFragmentId)?.configuration
+      ?.theme,
+  ])
+
+  return (
+    <div className="flex-1 h-full p-12">
+      <div className="grid grid-cols-3 gap-x-4 gap-y-4">
+        {Object.keys(Outros).map((key, index) => {
+          return (
+            <button
+              onClick={() => {
+                setOutro(`${index}`)
+                if (flick)
+                  setFlickStore((prev) => ({
+                    ...prev,
+                    flick: {
+                      ...flick,
+                      fragments: flick.fragments.map((fragment) => {
+                        if (fragment.id === activeFragmentId) {
+                          return {
+                            ...fragment,
+                            configuration: {
+                              theme: index,
+                            },
+                          }
+                        }
+                        return fragment
+                      }),
+                    },
+                  }))
+              }}
+              className={cx(
+                'bg-gray-100 p-10 rounded-md border border-transparent',
+                {
+                  'border-brand': outro === `${index}`,
+                }
+              )}
+              type="button"
+            >
+              <Text className="font-bold text-gray-800">{key}</Text>
+            </button>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
