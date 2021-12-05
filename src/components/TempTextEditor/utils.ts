52b7231d44ef9dc376dc33904e3e3cd8a7345906
Ishan Chhabra <me@ishanchhabra.com>
2021-12-05 21:48:51 +0530

    ✨ Create TextEditorParser

--- src/components/TempTextEditor/utils.ts
+++ src/components/TempTextEditor/utils.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+/* eslint-disable no-param-reassign */
+/* eslint-disable class-methods-use-this */
+import { SimpleAST } from './types'
+
 export function getCursorCoordinates(
   elem: HTMLDivElement,
   x: number,
@@ -6,4 +11,63 @@ export function getCursorCoordinates(
   const { left, top } = elem.getBoundingClientRect()
 
   return { x: x - left, y: y - top }
+}
+
+export class TextEditorParser {
+  ast: SimpleAST
+
+  constructor(ast: SimpleAST) {
+    this.ast = ast
+  }
+
+  private getCommonMd(title?: string, description?: string) {
+    if (title) {
+      title = `### ${title}`
+    }
+    return [title, description]
+      .filter((item) => typeof item !== 'undefined')
+      .join('\n\n ')
+  }
+
+  getMarkdown() {
+    const blocks = this.ast.blocks.filter(
+      (block) => typeof block.type !== 'undefined'
+    )
+
+    return blocks
+      .map((block) => {
+        let md = ''
+
+        md += this.getCommonMd(
+          // @ts-ignore
+          block[block['type']].title,
+          // @ts-ignore
+          block[block['type']].description
+        )
+
+        switch (block.type) {
+          case 'codeBlock':
+            md += `\n\n\`\`\`${block.codeBlock.language || ''}\n${
+              block.codeBlock.code
+            }\n\`\`\``
+            break
+          case 'imageBlock':
+            md += `\n\n![${block.imageBlock.title}](${block.imageBlock.url})`
+            break
+          case 'listBlock':
+            md += `\n\n${block.listBlock?.list
+              ?.map((item) => `- ${item.text}`)
+              .join('\n')}`
+            break
+          case 'videoBlock':
+            md += `\n\n![${block.videoBlock.title}](${block.videoBlock.url})`
+            break
+          default:
+            break
+        }
+
+        return md
+      })
+      .join('\n\n')
+  }
 }
