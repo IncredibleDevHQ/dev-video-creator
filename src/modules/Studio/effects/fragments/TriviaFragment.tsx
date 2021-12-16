0fced970ccae91fa43854cbd27933ad5c88ca155
Suvij Surya <suvijsurya76@gmail.com>
2021-12-16 15:11:15 +0530

    💄 Reposition the image/gif in trivia fragment

--- src/modules/Studio/effects/fragments/TriviaFragment.tsx
+++ src/modules/Studio/effects/fragments/TriviaFragment.tsx
@@ -138,22 +138,39 @@ const TriviaFragment = ({
     } else {
       setIsGif(false)
     }
-    if (triviaData?.text)
-      setImgDim(
-        getImageDimensions(
-          {
-            w: (qnaImage && qnaImage.width) || 0,
-            h: (qnaImage && qnaImage.height) || 0,
-          },
-          objectConfig.width - 30,
-          objectConfig.height - 140,
-          objectConfig.width - 40,
-          objectConfig.height,
-          20,
-          0
+    if (triviaData?.text) {
+      if (shortsMode) {
+        setImgDim(
+          getImageDimensions(
+            {
+              w: (qnaImage && qnaImage.width) || 0,
+              h: (qnaImage && qnaImage.height) || 0,
+            },
+            objectConfig.width - 30,
+            objectConfig.height - 140,
+            objectConfig.width - 40,
+            objectConfig.height,
+            20,
+            0
+          )
         )
-      )
-    else
+      } else {
+        setImgDim(
+          getImageDimensions(
+            {
+              w: (qnaImage && qnaImage.width) || 0,
+              h: (qnaImage && qnaImage.height) || 0,
+            },
+            objectConfig.width - 30,
+            objectConfig.height - 140,
+            objectConfig.width - 40,
+            objectConfig.height - 110,
+            20,
+            80
+          )
+        )
+      }
+    } else
       setImgDim(
         getImageDimensions(
           {
