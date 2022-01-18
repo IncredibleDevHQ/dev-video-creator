fac78f15c404cca3a81116856b378ed4127ba183
Ishan Chhabra <me@ishanchhabra.com>
2022-01-18 20:03:46 +0530

    ✨ Create useLoadFont

--- src/modules/Studio/hooks/index.ts
+++ src/modules/Studio/hooks/index.ts
@@ -4,5 +4,14 @@ import usePoint from './use-point'
 import useSplash from './use-splash'
 import useAgora from './use-agora-old'
 import useMediaStream from './use-media-stream'
+import useLoadFont from './use-load-font'
 
-export { useVideo, useAgora, useCode, usePoint, useSplash, useMediaStream }
+export {
+  useVideo,
+  useAgora,
+  useCode,
+  useLoadFont,
+  usePoint,
+  useSplash,
+  useMediaStream,
+}
