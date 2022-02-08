63e85df87d7cb97dec6735ff2c103935d24445e9
Suvij Surya <suvijsurya76@gmail.com>
2022-02-08 14:41:07 +0530

    💄 Add pastel lines video and fragment backgrounds

--- src/modules/Studio/components/VideoBackground.tsx
+++ src/modules/Studio/components/VideoBackground.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Image, Rect } from 'react-konva'
+import { Group, Image, Line, Rect } from 'react-konva'
 import { useRecoilValue } from 'recoil'
 import useImage from 'use-image'
 import config from '../../../config'
-import { VideoTheme } from '../../../utils/configTypes'
+import { ThemeFragment } from '../../../generated/graphql'
 import { studioStore } from '../stores'
 
 const VideoBackground = ({
   theme,
   stageConfig,
 }: {
-  theme: VideoTheme
+  theme: ThemeFragment
   stageConfig: {
     width: number
     height: number
@@ -22,8 +22,8 @@ const VideoBackground = ({
     `${config.storage.baseUrl}themes/glassyThemeBackground.png`,
     'anonymous'
   )
-  switch (theme) {
-    case 'glassy':
+  switch (theme.name) {
+    case 'DarkGradient':
       switch (branding?.background?.type) {
         case 'image':
           return (
@@ -58,7 +58,104 @@ const VideoBackground = ({
             />
           )
       }
-
+    case 'PastelLines':
+      switch (branding?.background?.type) {
+        case 'image':
+          return (
+            <Image
+              x={0}
+              y={0}
+              width={stageConfig.width}
+              height={stageConfig.height}
+              image={bgImage}
+              fill="#040E22"
+            />
+          )
+        case 'color':
+          return (
+            <Group>
+              <Rect
+                x={0}
+                y={0}
+                width={stageConfig.width}
+                height={stageConfig.height}
+                fill={branding?.background?.color?.primary}
+              />
+              <Line
+                points={[64, 0, 64, stageConfig.height]}
+                stroke={branding?.colors?.text || '#27272A'}
+                strokeWidth={1}
+              />
+              <Line
+                points={[
+                  stageConfig.width - 64,
+                  0,
+                  stageConfig.width - 64,
+                  stageConfig.height,
+                ]}
+                stroke={branding?.colors?.text || '#27272A'}
+                strokeWidth={1}
+              />
+              <Line
+                points={[0, 36, stageConfig.width, 36]}
+                stroke={branding?.colors?.text || '#27272A'}
+                strokeWidth={1}
+              />
+              <Line
+                points={[
+                  0,
+                  stageConfig.height - 36,
+                  stageConfig.width,
+                  stageConfig.height - 36,
+                ]}
+                stroke={branding?.colors?.text || '#27272A'}
+                strokeWidth={1}
+              />
+            </Group>
+          )
+        default:
+          return (
+            <Group>
+              <Rect
+                x={0}
+                y={0}
+                width={stageConfig.width}
+                height={stageConfig.height}
+                fill="#E0D6ED"
+              />
+              <Line
+                points={[64, 0, 64, stageConfig.height]}
+                stroke="#27272A"
+                strokeWidth={1}
+              />
+              <Line
+                points={[
+                  stageConfig.width - 64,
+                  0,
+                  stageConfig.width - 64,
+                  stageConfig.height,
+                ]}
+                stroke="#27272A"
+                strokeWidth={1}
+              />
+              <Line
+                points={[0, 36, stageConfig.width, 36]}
+                stroke="#27272A"
+                strokeWidth={1}
+              />
+              <Line
+                points={[
+                  0,
+                  stageConfig.height - 36,
+                  stageConfig.width,
+                  stageConfig.height - 36,
+                ]}
+                stroke="#27272A"
+                strokeWidth={1}
+              />
+            </Group>
+          )
+      }
     default:
       return <></>
   }
