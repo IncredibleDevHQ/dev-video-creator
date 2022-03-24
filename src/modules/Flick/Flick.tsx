fdf1a5367829779198f8344b9c5f58afecd8723b
Nithin Kumar B <nithin@incredible.dev>
2022-03-24 23:05:12 +0530

    ✨ Show preview for title

--- src/modules/Flick/Flick.tsx
+++ src/modules/Flick/Flick.tsx
@@ -545,9 +545,12 @@ const Flick = () => {
               className="h-full pt-12 pb-96 col-start-4 col-span-6"
             >
               <EditorHeader
+                blocks={simpleAST?.blocks || []}
+                setCurrentBlock={setCurrentBlock}
                 viewConfig={viewConfig}
                 setViewConfig={setViewConfig}
                 activeFragment={activeFragment}
+                setPreviewPosition={setPreviewPosition}
               />
 
               <TipTap
