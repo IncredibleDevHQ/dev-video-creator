97df469d87e53f1dd4dc1b059851b22c47640a8a
Suvij Surya <suvijsurya76@gmail.com>
2022-03-29 18:42:21 +0530

    🐛 Fix 'shows title as undefined if no title is provided'

--- src/modules/Studio/effects/fragments/PointsFragment.tsx
+++ src/modules/Studio/effects/fragments/PointsFragment.tsx
@@ -168,7 +168,7 @@ const PointsFragment = ({
   useEffect(() => {
     if (points.length === 0) return
     const noOflinesOfTitle = getNoOfLinesOfText({
-      text: dataConfig.listBlock.title || fragment?.name || '',
+      text: dataConfig.listBlock.title || '',
       availableWidth: objectRenderConfig.availableWidth - 80,
       fontSize: 40,
       fontFamily: branding?.font?.heading?.family || 'Gilroy',
@@ -316,7 +316,7 @@ const PointsFragment = ({
         }
         width={objectRenderConfig.availableWidth - 80}
         lineHeight={1.15}
-        text={dataConfig.listBlock.title || fragment?.name || ''}
+        text={dataConfig.listBlock.title || ''}
         fontStyle="normal 800"
         fontFamily={branding?.font?.heading?.family || 'Gilroy'}
       />
@@ -324,7 +324,9 @@ const PointsFragment = ({
         <Group
           x={objectRenderConfig.startX + 50}
           y={
-            appearance !== 'replace'
+            dataConfig?.listBlock?.title === undefined
+              ? objectRenderConfig.startY + 32
+              : appearance !== 'replace'
               ? objectRenderConfig.startY + 32 + 50 * titleNumberOfLines
               : objectRenderConfig.startY +
                 titleY +
