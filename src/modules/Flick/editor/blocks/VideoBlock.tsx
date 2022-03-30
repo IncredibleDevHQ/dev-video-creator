581b17529450f230e76436af7524ed989e6da4af
Nithin Kumar B <nithin@incredible.dev>
2022-03-30 16:27:29 +0530

    ✨ Add screen grab as a slash command

--- src/modules/Flick/editor/blocks/VideoBlock.tsx
+++ src/modules/Flick/editor/blocks/VideoBlock.tsx
@@ -233,16 +233,24 @@ const VideoBlock = (props: any) => {
               <FiUploadCloud size={24} className="my-2" />
 
               <div className="z-50 text-center text-black">
-                <Text contentEditable={false} fontSize="small">
-                  Drag and drop {props.node.attrs.type} or
-                </Text>
-                <Text
-                  contentEditable={false}
-                  fontSize="small"
-                  className="font-semibold"
-                >
-                  browse
-                </Text>
+                {props.node.attrs.type === 'video' ? (
+                  <>
+                    <Text contentEditable={false} fontSize="small">
+                      Drag and drop {props.node.attrs.type} or
+                    </Text>
+                    <Text
+                      contentEditable={false}
+                      fontSize="small"
+                      className="font-semibold"
+                    >
+                      browse
+                    </Text>
+                  </>
+                ) : (
+                  <Text contentEditable={false} fontSize="small">
+                    Click to record screen
+                  </Text>
+                )}
               </div>
             </div>
           )}
@@ -256,7 +264,7 @@ const VideoBlock = (props: any) => {
                 props.node.attrs['data-transformations']
               ),
             }}
-            shouldResetWhenOpened={retakeVideo}
+            shouldResetWhenOpened={props.node.attrs.type === 'video'}
             handleClose={() => {
               setEditVideo(false)
               setRetakeVideo(false)
@@ -267,6 +275,7 @@ const VideoBlock = (props: any) => {
                 'data-transformations': JSON.stringify(transformations),
               })
             }}
+            recordScreenMode
           />
         )}
       </NodeViewWrapper>
@@ -499,6 +508,9 @@ export default Node.create({
       },
       caption: {
         default: null,
+      },
+      type: {
+        default: 'video', // video, screengrab
       },
     }
   },
