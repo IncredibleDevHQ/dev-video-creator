32b09aa9d90bd16b2ad9322c5f2df366dddca45a
Suvij Surya <suvijsurya76@gmail.com>
2022-04-06 02:34:46 +0530

    💄 Add intro for lambdatest theme

--- src/modules/Studio/effects/Splashes/LambdaTestSplash.tsx
+++ src/modules/Studio/effects/Splashes/LambdaTestSplash.tsx
@@ -2,12 +2,13 @@ import React, { SetStateAction, useEffect, useState } from 'react'
 import { Group, Image, Text } from 'react-konva'
 import { useImage } from 'react-konva-utils'
 import { useRecoilValue } from 'recoil'
+import FragmentBackground from '../../components/FragmentBackground'
 import VideoBackground from '../../components/VideoBackground'
 import useEdit from '../../hooks/use-edit'
 import { studioStore } from '../../stores'
 import { getThemeTextColor } from '../../utils/ThemeConfig'
 
-const GlassySplash = ({
+const LambdaTestSplash = ({
   isShorts,
   stageConfig,
   setIsTitleSplash,
@@ -21,6 +22,7 @@ const GlassySplash = ({
 }) => {
   const { fragment, branding, theme } = useRecoilValue(studioStore)
   const [logo] = useImage(branding?.logo || '', 'anonymous')
+  const { clipRect } = useEdit()
 
   const [imgDim, setImgDim] = useState<{
     width: number
@@ -54,10 +56,56 @@ const GlassySplash = ({
         stageConfig={stageConfig}
         isShorts={isShorts}
       />
+      <Group
+        clipFunc={(ctx: any) => {
+          clipRect(
+            ctx,
+            !isShorts
+              ? {
+                  x: 56,
+                  y: 32,
+                  width: 848,
+                  height: 477,
+                  borderRadius: 16,
+                }
+              : {
+                  x: 16,
+                  y: 16,
+                  width: 364,
+                  height: 672,
+                  borderRadius: 16,
+                }
+          )
+        }}
+      >
+        <FragmentBackground
+          theme={theme}
+          objectConfig={
+            !isShorts
+              ? {
+                  x: 56,
+                  y: 32,
+                  width: 848,
+                  height: 477,
+                  borderRadius: 16,
+                }
+              : {
+                  x: 16,
+                  y: 16,
+                  width: 364,
+                  height: 672,
+                  borderRadius: 16,
+                }
+          }
+          backgroundRectColor={
+            branding?.colors?.primary ? branding?.colors?.primary : '#ffffff'
+          }
+        />
+      </Group>
       <Text
-        x={!isShorts ? 40 : 20}
+        x={!isShorts ? 96 : 32}
         y={0}
-        width={!isShorts ? 600 : 350}
+        width={!isShorts ? 600 : 320}
         height={stageConfig.height}
         verticalAlign="middle"
         text={fragment?.flick.name || 'Hello Intro'}
@@ -76,8 +124,8 @@ const GlassySplash = ({
         }}
       />
       <Image
-        x={40}
-        y={stageConfig.height - 90}
+        x={!isShorts ? 96 : 32}
+        y={!isShorts ? stageConfig.height - 120 : stageConfig.height - 90}
         width={imgDim.width}
         height={imgDim.height}
         image={logo}
@@ -86,4 +134,4 @@ const GlassySplash = ({
   )
 }
 
-export default GlassySplash
+export default LambdaTestSplash
