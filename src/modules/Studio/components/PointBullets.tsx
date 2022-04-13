4ff2698a90b5fb078d1932c1486f35c6f8d824af
Suvij Surya <suvijsurya76@gmail.com>
2022-04-14 03:36:58 +0530

    ✨ Add point bullets component

---
+++ src/modules/Studio/components/PointBullets.tsx
@@ -0,0 +1,142 @@
+import React from 'react'
+import { Rect, Text } from 'react-konva'
+import { useRecoilValue } from 'recoil'
+import { studioStore } from '../stores'
+import { BulletsConfig } from '../utils/PointsConfig'
+
+const PointBullets = ({
+  theme,
+  bulletsConfig,
+  pointNumber,
+}: {
+  theme: string
+  bulletsConfig: BulletsConfig
+  pointNumber: number
+}) => {
+  const { branding } = useRecoilValue(studioStore)
+  switch (theme) {
+    case 'DarkGradient':
+    case 'PastelLines':
+    case 'Cassidoo':
+    case 'LambdaTest':
+      return (
+        <>
+          <Rect
+            x={bulletsConfig.bulletXOffset}
+            y={bulletsConfig.bulletYOffset}
+            width={bulletsConfig.bulletWidth}
+            height={bulletsConfig.bulletHeight}
+            fill={(bulletsConfig.bulletColor as string) || 'white'}
+            cornerRadius={bulletsConfig.bulletCornerRadius}
+            offsetX={bulletsConfig.bulletXOffset}
+            offsetY={bulletsConfig.bulletYOffset}
+            rotation={bulletsConfig.bulletRotation}
+          />
+          <Text
+            text={pointNumber.toString()}
+            fontSize={bulletsConfig.bulletFontSize}
+            fill={bulletsConfig.bulletTextColor || 'black'}
+            fontFamily={branding?.font?.body?.family || 'Inter'}
+            width={bulletsConfig.bulletWidth}
+            height={bulletsConfig.bulletHeight}
+            align="center"
+            verticalAlign="middle"
+          />
+        </>
+      )
+    case 'LeeRob':
+      return (
+        <>
+          {/* <Line
+            points={[
+              -1920,
+              bulletsConfig.bulletHeight / 2,
+              1920,
+              bulletsConfig.bulletHeight / 2,
+            ]}
+            stroke="#ffffff19"
+            strokeWidth={1}
+          /> */}
+          <Rect
+            x={bulletsConfig.bulletXOffset}
+            y={bulletsConfig.bulletYOffset}
+            width={bulletsConfig.bulletWidth}
+            height={bulletsConfig.bulletHeight}
+            fill={(bulletsConfig.bulletColor as string) || 'white'}
+            cornerRadius={bulletsConfig.bulletCornerRadius}
+            offsetX={bulletsConfig.bulletXOffset}
+            offsetY={bulletsConfig.bulletYOffset}
+            rotation={bulletsConfig.bulletRotation}
+          />
+          <Text
+            text={pointNumber.toString()}
+            fontSize={bulletsConfig.bulletFontSize}
+            fill={bulletsConfig.bulletTextColor || 'black'}
+            fontFamily={branding?.font?.body?.family || 'Inter'}
+            width={bulletsConfig.bulletWidth}
+            height={bulletsConfig.bulletHeight}
+            align="center"
+            verticalAlign="middle"
+          />
+        </>
+      )
+    case 'Web3Auth':
+      return (
+        <>
+          <Rect
+            x={bulletsConfig.bulletBgRectXOffset}
+            y={bulletsConfig.bulletBgRectYOffset}
+            width={bulletsConfig.bulletBgRectWidth}
+            height={bulletsConfig.bulletBgRectHeight}
+            fill={
+              (bulletsConfig.bulletBgRectColor as string[])[
+                (pointNumber - 1) % 4
+              ] || 'white'
+            }
+            opacity={0.1}
+            cornerRadius={bulletsConfig.bulletBgRectCornerRadius}
+          />
+          <Rect
+            x={bulletsConfig.bulletBgRectXOffset}
+            y={bulletsConfig.bulletBgRectYOffset}
+            width={bulletsConfig.bulletBgRectWidth}
+            height={bulletsConfig.bulletBgRectHeight}
+            stroke={
+              (bulletsConfig.bulletBgRectColor as string[])[
+                (pointNumber - 1) % 4
+              ] || 'white'
+            }
+            cornerRadius={bulletsConfig.bulletBgRectCornerRadius}
+          />
+          <Rect
+            x={bulletsConfig.bulletXOffset}
+            y={bulletsConfig.bulletYOffset}
+            width={bulletsConfig.bulletWidth}
+            height={bulletsConfig.bulletHeight}
+            fill={
+              (bulletsConfig.bulletColor as string[])[(pointNumber - 1) % 4] ||
+              'white'
+            }
+            cornerRadius={bulletsConfig.bulletCornerRadius}
+            offsetX={bulletsConfig.bulletXOffset}
+            offsetY={bulletsConfig.bulletYOffset}
+            rotation={bulletsConfig.bulletRotation}
+          />
+          <Text
+            text={pointNumber.toString()}
+            fontSize={bulletsConfig.bulletFontSize}
+            fill={bulletsConfig.bulletTextColor || 'black'}
+            fontFamily={branding?.font?.body?.family || 'Inter'}
+            width={bulletsConfig.bulletWidth}
+            height={bulletsConfig.bulletHeight}
+            align="center"
+            verticalAlign="middle"
+          />
+        </>
+      )
+    default:
+      return <></>
+  }
+}
+
+export default PointBullets
