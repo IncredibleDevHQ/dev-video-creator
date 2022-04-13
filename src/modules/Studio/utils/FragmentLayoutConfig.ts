434d7776bd02ceb8dbcae229287a0a9f48471e9a
Suvij Surya <suvijsurya76@gmail.com>
2022-04-14 03:35:00 +0530

    🔧 Add fragment and studiouser config for LeeRob's and Web3Auth's theme

--- src/modules/Studio/utils/FragmentLayoutConfig.ts
+++ src/modules/Studio/utils/FragmentLayoutConfig.ts
@@ -193,6 +193,52 @@ export const FragmentLayoutConfig = ({
               borderRadius: 16,
             }
         }
+      case 'LeeRob':
+        switch (layout) {
+          case 'classic':
+          case 'padded-bottom-right-circle':
+          case 'padded-bottom-right-tile':
+            return {
+              x: 16,
+              y: 16,
+              width: 364,
+              height: 672,
+              borderRadius: 0,
+            }
+          case 'bottom-right-tile':
+          case 'bottom-right-circle':
+            return {
+              x: 0,
+              y: 0,
+              width: 396,
+              height: 704,
+              borderRadius: 0,
+            }
+          case 'split':
+            return {
+              x: 16,
+              y: 12,
+              width: 364,
+              height: 336,
+              borderRadius: 0,
+            }
+          case 'full-left':
+            return {
+              x: 16,
+              y: 24,
+              width: 364,
+              height: 280,
+              borderRadius: 0,
+            }
+          default:
+            return {
+              x: 16,
+              y: 16,
+              width: 364,
+              height: 672,
+              borderRadius: 0,
+            }
+        }
       default:
         return {
           x: 16,
@@ -553,6 +599,83 @@ export const FragmentLayoutConfig = ({
             width: 848,
             height: 477,
             borderRadius: 16,
+          }
+      }
+    case 'LeeRob':
+    case 'Web3Auth':
+      switch (layout) {
+        case 'classic':
+          return {
+            x: 56,
+            y: 32,
+            width: 848,
+            height: 477,
+            borderRadius: 0,
+          }
+        case 'float-full-right':
+          return {
+            x: 32,
+            y: 90,
+            width: 640,
+            height: 360,
+            borderRadius: 0,
+          }
+        case 'float-full-left':
+          return {
+            x: 288,
+            y: 90,
+            width: 640,
+            height: 360,
+            borderRadius: 0,
+          }
+        case 'float-half-right':
+          return {
+            x: 32,
+            y: 45,
+            width: 800,
+            height: 450,
+            availableWidth: 650,
+            borderRadius: 0,
+          }
+        case 'padded-bottom-right-tile':
+          return {
+            x: 72,
+            y: 41,
+            width: 816,
+            height: 459,
+            borderRadius: 0,
+          }
+        case 'padded-split':
+          return {
+            x: 40,
+            y: 112.5,
+            width: 560,
+            height: 315,
+            borderRadius: 0,
+          }
+        case 'full-left':
+          return {
+            x: 40,
+            y: 120,
+            width: 500,
+            height: 300,
+            borderRadius: 0,
+          }
+        case 'full-right':
+          return {
+            x: 420,
+            y: 120,
+            width: 500,
+            height: 300,
+            borderRadius: 0,
+          }
+        default:
+          return {
+            x: 56,
+            y: 32,
+            width: 848,
+            height: 477,
+            borderRadius: 0,
           }
       }
     default:
