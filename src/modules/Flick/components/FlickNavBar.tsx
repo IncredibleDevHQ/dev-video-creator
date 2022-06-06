93e77414ac8bb28e6b122ea09a2030ad4a44b3aa
Aakash <9267aakashsharma@gmail.com>
2022-06-06 18:14:36 +0530

    🚧 Add huddle to notebook

--- src/modules/Flick/components/FlickNavBar.tsx
+++ src/modules/Flick/components/FlickNavBar.tsx
@@ -1,15 +1,133 @@
-import React, { useState } from 'react'
+import AgoraRTC from 'agora-rtc-sdk-ng'
+import { createMicrophoneAudioTrack } from 'agora-rtc-react'
+import React, { useEffect, useState } from 'react'
 import { FiChevronLeft } from 'react-icons/fi'
 import { IoPeopleOutline } from 'react-icons/io5'
-import { useRecoilState } from 'recoil'
-import { Button, Heading } from '../../../components'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { Button, emitToast, Heading, Text } from '../../../components'
 import { ASSETS } from '../../../constants'
+import { useGetHuddleRtcTokenMutation } from '../../../generated/graphql'
+import { userState } from '../../../stores/user.store'
+import useAudio from '../hooks/use-audio'
 import { newFlickStore } from '../store/flickNew.store'
 import ShareModal from './ShareModal'
+
+const FlickHuddle = ({
+  flickId,
+  devices,
+  deviceId,
+}: {
+  flickId: string
+  deviceId?: string
+  devices: MediaDeviceInfo[]
+}) => {
+  const user = useRecoilValue(userState)
+  const useTrack = createMicrophoneAudioTrack(
+    deviceId
+      ? {
+          microphoneId: deviceId,
+        }
+      : undefined
+  )
+  const { ready: trackReady, error: trackError, track } = useTrack()
+  const [getHuddleToken, { data: rtcTokenData }] =
+    useGetHuddleRtcTokenMutation()
+  const {
+    init,
+    mute,
+    join,
+    users,
+    leave,
+    renewToken,
+    ready: agoraReady,
+  } = useAudio()
+
+  useEffect(() => {
+    if (!agoraReady || !user?.sub || !track) return
+    ;(async () => {
+      try {
+        if (!user?.sub || !rtcTokenData?.HuddleRtcToken?.token) return
+        await join(rtcTokenData.HuddleRtcToken.token, user.sub, track)
+      } catch (error: any) {
+        emitToast({
+          type: 'error',
+          title: 'Failed to initialize AgoraRTC',
+          description: error?.message,
+        })
+      }
+    })()
+  }, [agoraReady, user?.sub, track, rtcTokenData?.HuddleRtcToken?.token])
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        if (!trackReady || !track) return
+        await init(
+          flickId,
+          {
+            onTokenWillExpire: async () => {
+              const { data } = await getHuddleToken({
+                variables: { flickId },
+              })
+              if (data?.HuddleRtcToken?.token) {
+                renewToken(data?.HuddleRtcToken?.token)
+              }
+            },
+            onTokenDidExpire: async () => {
+              const { data } = await getHuddleToken({
+                variables: { flickId },
+              })
+              if (data?.HuddleRtcToken?.token) {
+                join(data?.HuddleRtcToken?.token, user?.sub as string, track)
+              }
+            },
+          },
+          track
+        )
+      } catch (error: any) {
+        emitToast({
+          type: 'error',
+          title: 'Failed to initialize AgoraRTC',
+          description: error?.message,
+        })
+      }
+    })()
+  }, [trackReady, track])
+
+  if (trackError) return <div>{trackError.message}</div>
+
+  return <div>Hi</div>
+}
 
 const FlickNavBar = () => {
   const [{ flick }] = useRecoilState(newFlickStore)
   const [isShareOpen, setIsShareOpen] = useState(false)
+
+  const [audioDevices, setAudioDevices] = useState<MediaDeviceInfo[]>([])
+  const [currentAudioDevice, setCurrentAudioDevice] =
+    useState<MediaDeviceInfo>()
+
+  const [inHuddle, setInHuddle] = useState(false)
+
+  const joinHuddle = async () => {
+    try {
+      if (!flick?.id) return
+
+      // NOTE - Get microphone devices
+      const devices = await AgoraRTC.getMicrophones()
+
+      // NOTE - Set audio devices
+      setAudioDevices(devices)
+      setCurrentAudioDevice(devices[0])
+      setInHuddle(true)
+    } catch (error: any) {
+      emitToast({
+        type: 'error',
+        title: 'Error',
+        description: error?.message || 'Error joining huddle',
+      })
+    }
+  }
 
   return (
     <div className="sticky top-0 z-50 flex items-center justify-between py-2 pl-3 pr-5 bg-dark-500">
@@ -24,7 +142,38 @@ const FlickNavBar = () => {
       <Heading className="p-2 ml-12 font-bold text-base text-white">
         {flick?.name || ''}
       </Heading>
-      <div className="flex items-stretch gap-x-6 px-2">
+      <div className="flex justify-end items-center gap-x-2 px-2">
+        {inHuddle ? (
+          <FlickHuddle
+            flickId={flick?.id}
+            devices={audioDevices}
+            deviceId={currentAudioDevice?.deviceId}
+          />
+        ) : (
+          <Button
+            appearance="primary"
+            type="button"
+            onClick={joinHuddle}
+            size="small"
+            className="mr-2"
+          >
+            <Text className="mr-2 text-sm">Join</Text>
+            <div className="flex justify-end items-center">
+              {flick?.participants.map((participant, index) => (
+                <div
+                  key={participant.id || index}
+                  className="flex items-center"
+                >
+                  <img
+                    src={participant.user.picture || ''}
+                    alt={participant.user.displayName || ''}
+                    className="w-6 h-6 rounded-full"
+                  />
+                </div>
+              ))}
+            </div>
+          </Button>
+        )}
         <Button
           appearance="gray"
           type="button"
---
+++ src/modules/Flick/hooks/use-audio.ts
@@ -0,0 +1,162 @@
+import { createClient } from 'agora-rtc-react'
+import AgoraRTC, {
+  ClientConfig,
+  IAgoraRTCRemoteUser,
+  IMicrophoneAudioTrack,
+} from 'agora-rtc-sdk-ng'
+import { useState } from 'react'
+import config from '../../../config'
+
+export interface RTCUser extends IAgoraRTCRemoteUser {
+  audioStream?: MediaStream
+}
+
+const audioConfig: ClientConfig = {
+  mode: 'rtc',
+  codec: 'h264',
+}
+
+const { appId } = config.agora
+
+console.log('app id', appId)
+
+const useClient = createClient(audioConfig)
+
+const useAudio = () => {
+  const client = useClient()
+  const [ready, setReady] = useState(false)
+  const [users, setUsers] = useState<RTCUser[]>([])
+  const [channel, setChannel] = useState<string>()
+
+  const [audioTrack, setAudioTrack] = useState<IMicrophoneAudioTrack>()
+
+  const init = async (
+    channel: string,
+    {
+      onTokenWillExpire,
+      onTokenDidExpire,
+    }: { onTokenWillExpire: () => void; onTokenDidExpire: () => void },
+    track: IMicrophoneAudioTrack
+  ) => {
+    try {
+      setReady(false)
+      setChannel(channel)
+      setAudioTrack(track)
+
+      client.on('user-published', async (user, mediaType) => {
+        await client.subscribe(user, mediaType)
+        const tracks: MediaStreamTrack[] = []
+        if (user.audioTrack) tracks.push(user.audioTrack?.getMediaStreamTrack())
+        if (mediaType === 'audio') {
+          user.audioTrack?.play()
+          setUsers((prevUsers) => {
+            if (prevUsers.find((element) => element.uid === user.uid))
+              return [...prevUsers]
+            return [
+              ...prevUsers,
+              {
+                ...user,
+                muted: false,
+                mediaStream:
+                  tracks && tracks.length > 0
+                    ? new MediaStream(tracks.filter((track) => !!track))
+                    : undefined,
+              },
+            ]
+          })
+        }
+      })
+
+      client.on('user-left', (user) => {
+        setUsers((prevUsers) => {
+          return prevUsers.filter((User) => User.uid !== user.uid)
+        })
+      })
+
+      client.on('token-privilege-will-expire', () => {
+        onTokenWillExpire()
+      })
+
+      client.on('token-privilege-did-expire', () => {
+        onTokenDidExpire()
+      })
+      setReady(true)
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  AgoraRTC.onMicrophoneChanged = async (changedDevice) => {
+    if (changedDevice.state === 'ACTIVE') {
+      await audioTrack?.setDevice(changedDevice.device.deviceId)
+      // Switch to an existing device when the current device is unplugged.
+    } else if (changedDevice.device.label === audioTrack?.getTrackLabel()) {
+      const oldMicrophones = await AgoraRTC.getMicrophones()
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      await audioTrack?.setDevice(oldMicrophones?.[0]?.deviceId)
+    }
+  }
+
+  const renewToken = async (token: string) => {
+    client.renewToken(token)
+  }
+
+  const join = async (
+    token: string,
+    uid: string,
+    mediaTracks: IMicrophoneAudioTrack | null
+  ) => {
+    try {
+      if (!channel || !ready) return
+      console.log('joining channel', { appId, channel, token, uid })
+      await client.join(appId, channel, token, uid)
+      if (mediaTracks) await client.publish(mediaTracks)
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  }
+
+  const mute = async (uid: string) => {
+    try {
+      if (!ready) return
+      const tempUsers = [...users]
+      const currentUserIndex = tempUsers.findIndex((user) => user.uid === uid)
+      if (currentUserIndex === -1) return
+      tempUsers.splice(currentUserIndex, 1, {
+        ...tempUsers[currentUserIndex],
+        hasAudio: !tempUsers[currentUserIndex].hasAudio,
+      })
+      if (audioTrack)
+        audioTrack.setEnabled(!tempUsers[currentUserIndex].hasAudio)
+      else throw new Error('No audio track')
+      setUsers(tempUsers)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const leave = async () => {
+    try {
+      if (!ready) return
+      audioTrack?.stop()
+      await client.leave()
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  }
+
+  return {
+    init,
+    ready,
+    users,
+    join,
+    mute,
+    leave,
+    renewToken,
+  }
+}
+
+export default useAudio
