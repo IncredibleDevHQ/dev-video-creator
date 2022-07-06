66f337cce7c7eb6e46a292128bacff40dc1b4f5f
Pythonista7 <ashwin.mirskar@gmail.com>
2022-07-06 19:08:33 +0530

    fix(fix duplicate getenv export): remove duplicate getEnv export

--- apps/webfront/src/utils/env.ts
+++ apps/webfront/src/utils/env.ts
@@ -1,35 +1,33 @@
 import crypto from 'crypto'
 
-const decryptEnvs = (encrypted: string) => {
-	const algo = process.env.ENV_ENCRYPTION_ALGORITHM?.toString()
-	const key = process.env.ENCRYPTION_KEY?.toString()
-	const iv = process.env.IV?.toString()
-
-	if (!key || !iv || !algo) {
-		throw new Error('Invalid encryption configuration')
-	}
-
-	const decipher = crypto.createDecipheriv(
-		algo || `aes-256-cbc`,
-		key, // ENC_KEY
-		iv // IV
-	)
-	const decrypted = decipher.update(encrypted, 'base64', 'utf8')
-	return decrypted + decipher.final('utf8')
-}
-
 export type EnvType = {
 	[key: string]: string
 }
 
-const getEnvs = (encrypted: string): EnvType => {
-	const decrypted = decryptEnvs(encrypted)
+const decryptEnvs = (encrypted: string): EnvType => {
 	try {
+		const algo = process.env.ENV_ENCRYPTION_ALGORITHM?.toString()
+		const key = process.env.ENCRYPTION_KEY?.toString()
+		const iv = process.env.IV?.toString()
+
+		if (!key || !iv || !algo) {
+			throw new Error('Invalid encryption configuration')
+		}
+
+		const decipher = crypto.createDecipheriv(
+			algo || `aes-256-cbc`,
+			key, // ENC_KEY
+			iv // IV
+		)
+
+		let decrypted = decipher.update(encrypted, 'base64', 'utf8')
+		decrypted += decipher.final('utf8')
+
 		const envs: EnvType = JSON.parse(decrypted)
 		return envs
 	} catch (e) {
-		throw new Error('Invalid encrypted env')
+		throw new Error(`Invalid encrypted env. Error: ${e}`)
 	}
 }
 
-export default getEnvs
+export default decryptEnvs
