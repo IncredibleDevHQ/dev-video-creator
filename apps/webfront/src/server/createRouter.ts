b4e9000a23426ba1056d0a01ff557f11b5364f32
Pythonista7 <ashwin.mirskar@gmail.com>
2022-07-10 10:29:15 +0530

    refactor(webfront/trpc): init server/helpers
    move commonly used functions/types to helper file.

--- apps/webfront/src/server/createRouter.ts
+++ apps/webfront/src/server/createRouter.ts
@@ -1,7 +1,7 @@
 import { router } from '@trpc/server'
 import superjson from 'superjson'
 import { Context } from './createContext'
-import { Meta } from './routes/userRouter'
+import { Meta } from './utils/helpers'
 
 function createRouter() {
 	return router<Context, Meta>().transformer(superjson)
