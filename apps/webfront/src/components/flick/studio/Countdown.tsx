983faa32a5bd27f5e19e325795da2c77a9399b4a
Suvij Surya <suvijsurya76@gmail.com>
2022-07-12 16:28:15 +0530

    feat(webfront): update state to recording on finish of the countdown

--- apps/webfront/src/components/flick/studio/Countdown.tsx
+++ apps/webfront/src/components/flick/studio/Countdown.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
-import { studioStateAtom } from 'src/stores/studio.store'
+import { StudioState, studioStateAtom } from 'src/stores/studio.store'
 import { Text } from 'ui/src/Text'
 
-const Countdown = () => {
+const Countdown = ({ updateState }: { updateState: (value: StudioState) => void }) => {
 	const state = useRecoilValue(studioStateAtom)
 	const [timer, setTimer] = useState<number>(3)
 
 	useEffect(() => {
 		if (state === 'countDown') {
 			if (timer === 0) {
-				// TODO change state to recording
+        updateState('recording')
 			} else {
 				setTimeout(() => {
 					setTimer(timer - 1)
