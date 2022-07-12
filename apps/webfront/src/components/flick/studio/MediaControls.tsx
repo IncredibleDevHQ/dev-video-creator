e99cdf3c0e6df70eebd2ddb8a44056a7dff1b3ac
Suvij Surya <suvijsurya76@gmail.com>
2022-07-12 16:31:35 +0530

    feat(webfront): (WIP) Media controls

--- apps/webfront/src/components/flick/studio/MediaControls.tsx
+++ apps/webfront/src/components/flick/studio/MediaControls.tsx
@@ -1,4 +1,8 @@
-const MediaControls = () =>
+import { createMicrophoneAndCameraTracks } from 'agora-rtc-react'
+// import useMediaStream from 'src/utils/hooks/useMediaStream'
+
+const MediaControls = () => {
+
 	// const {
 	// 	// camera,
 	// 	// permissions,
@@ -27,11 +31,67 @@ const MediaControls = () =>
 	// 	)
 	// }, [ready])
 
-	 (
+	// Gives the tracks of camera and microphone, given the camera and microphone device ids
+	const { tracks, error } = createMicrophoneAndCameraTracks(
+		{
+			microphoneId: localStorage.getItem('preferred-microphone') || '',
+		},
+		{
+			cameraId: localStorage.getItem('preferred-camera') || '',
+			encoderConfig: {
+				width: 960,
+				height: 720,
+				frameRate: 60,
+				bitrateMax: 3000,
+				bitrateMin: 2200,
+			},
+		}
+	)()
+
+  // const { stream, join, users, leave, userAudios, renewToken } = useAgora(
+	// 	fragmentId,
+	// 	{
+	// 		onTokenWillExpire: async () => {
+	// 			const { data } = await getRTCToken({
+	// 				variables: { fragmentId, flickId },
+	// 			})
+	// 			if (data?.RTCToken?.token) {
+	// 				renewToken(data.RTCToken.token)
+	// 			}
+	// 		},
+	// 		onTokenDidExpire: async () => {
+	// 			const { data } = await getRTCToken({
+	// 				variables: { fragmentId, flickId },
+	// 			})
+	// 			if (data?.RTCToken?.token) {
+	// 				const participantId = fragment?.configuration?.speakers?.find(
+	// 					({ participant }: { participant: FlickParticipantsFragment }) =>
+	// 						participant.userSub === sub
+	// 				)?.participant.id
+	// 				if (participantId) {
+	// 					join(data?.RTCToken?.token, participantId as string, tracks)
+	// 				} else {
+	// 					leave()
+	// 					clearRecordedBlocks()
+	// 					emitToast({
+	// 						title: 'Yikes. Something went wrong.',
+	// 						type: 'error',
+	// 						description:
+	// 							'You do not belong to this studio!! Please ask the host to invite you again.',
+	// 					})
+	// 					history.goBack()
+	// 				}
+	// 			}
+	// 		},
+	// 	},
+	// 	tracks
+	// )
+
+  console.log('MediaControls', tracks, error)
+	return (
 		<div className='bg-zinc-700/90 h-8 rounded-3xl text-cyan-50 px-4 py-1'>
 			Media
 		</div>
 	)
-
-
+}
 export default MediaControls
