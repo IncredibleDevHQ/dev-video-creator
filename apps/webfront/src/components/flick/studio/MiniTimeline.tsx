58a7ebc8185ef5414ceed666f06340e41c602f0f
Suvij Surya <suvijsurya76@gmail.com>
2022-07-13 02:57:34 +0530

    feat(webfront): add minitimeline functionality

--- apps/webfront/src/components/flick/studio/MiniTimeline.tsx
+++ apps/webfront/src/components/flick/studio/MiniTimeline.tsx
@@ -1,7 +1,15 @@
 import { css, cx } from '@emotion/css'
 import { Block } from 'editor/src/utils/types'
 import { useRef } from 'react'
+import { IoCheckmarkOutline } from 'react-icons/io5'
+import { useRecoilValue } from 'recoil'
+import {
+	activeObjectIndexAtom,
+	StudioState,
+	studioStateAtom,
+} from 'src/stores/studio.store'
 import useUpdateActiveObjectIndex from 'src/utils/hooks/useUpdateActiveObjectIndex'
+import { useMap } from 'src/utils/liveblocks.config'
 
 const noScrollBar = css`
 	::-webkit-scrollbar {
@@ -34,13 +42,17 @@ export const getBlockTitle = (block: Block): string => {
 
 const MiniTimeline = ({
 	dataConfig,
-}: // continuousRecording,
-{
+	continuousRecording,
+	updateState,
+}: {
 	dataConfig: Block[]
-	// continuousRecording: boolean
+	continuousRecording: boolean
+	updateState: (studioState: StudioState) => void
 }) => {
 	const timelineRef = useRef<HTMLDivElement>(null)
-	// const state = useRecoilValue(studioStateAtom)
+	const state = useRecoilValue(studioStateAtom)
+	const activeObjectIndex = useRecoilValue(activeObjectIndexAtom)
+	const recordedBlocks = useMap('recordedBlocks')
 	const { updateActiveObjectIndex } = useUpdateActiveObjectIndex(true)
 
 	return (
@@ -56,80 +68,59 @@ const MiniTimeline = ({
 			}}
 			className={cx(
 				'mt-auto flex gap-x-3 px-6 py-2 overflow-x-scroll h-12',
-				// {
-				// 	'pointer-events-none':
-				// 		state === 'preview' && recordedBlocks && currentBlock
-				// 			? recordedBlocks
-				// 					?.find(b => b.id === currentBlock.id)
-				// 					?.objectUrl?.includes('blob') || false
-				// 			: false,
-				// },
+				{
+					'pointer-events-none':
+						state === 'preview'
+							? recordedBlocks
+									?.get(dataConfig[activeObjectIndex].id)
+									?.includes('blob') || false
+							: false,
+				},
 				noScrollBar
 			)}
 		>
-			{dataConfig.map((block,index) => (
+			{dataConfig.map((block, index) => (
 				<button
 					type='button'
 					id={`timeline-block-${block.id}`}
 					className={cx(
-						'px-3 py-1.5 h-8 font-body cursor-pointer text-size-sm rounded-sm flex items-center justify-center transition-transform duration-500 bg-gray-700 relative text-gray-300 flex-shrink-0'
-						// {
-						// 	'transform scale-110 border border-brand':
-						// 		payload?.activeObjectIndex === index,
-						// 	'bg-grey-900 text-gray-500': index !== payload?.activeObjectIndex,
-						// 	'cursor-not-allowed':
-						// 		state === 'recording' ||
-						// 		state === 'start-recording' ||
-						// 		(recordedBlocks
-						// 			?.find(b => b?.id === currentBlock?.id)
-						// 			?.objectUrl?.includes('blob') &&
-						// 			state === 'preview'),
-
-						// 	// state !== 'ready' || state !== 'preview',
-						// }
+						'px-3 py-1.5 h-8 font-body cursor-pointer text-size-sm rounded-sm flex items-center justify-center transition-transform duration-500 bg-gray-700 relative text-gray-300 flex-shrink-0',
+						{
+							'transform scale-110 border border-brand':
+								activeObjectIndex === index,
+							'bg-grey-900 text-gray-500': index !== activeObjectIndex,
+							'cursor-not-allowed':
+								state === 'recording' ||
+								(recordedBlocks?.get(dataConfig[index].id)?.includes('blob') &&
+									state === 'preview'),
+						}
 					)}
 					onClick={() => {
-            updateActiveObjectIndex(index)
 						// if (payload?.studioControllerSub !== sub) return
-						// // if continuous recording is enabled, disable mini-timeline onclick
-						// if (
-						// 	continuousRecording &&
-						// 	(state === 'recording' ||
-						// 		state === 'start-recording' ||
-						// 		state === 'preview')
-						// ) {
-						// 	return
-						// }
-						// // maybe this is not the best thing to do , can actually be a feature.
-						// // TODO: if current block is recorded by isnt saved to the cloud or if the user has not intentionally pressed retake to discard the rec, show warning.
-						// const newSrc =
-						// 	recordedBlocks && currentBlock
-						// 		? recordedBlocks?.find(b => b.id === currentBlock.id)
-						// 				?.objectUrl || ''
-						// 		: ''
-						// if (newSrc.includes('blob') && state === 'preview') return
-						// // checking if block already has recording
-						// const clickedBlock = recordedBlocks?.find(b => b.id === block.id)
-						// updatePayload({
-						// 	activeObjectIndex: index,
-						// })
-						// console.log('clickedBlock', clickedBlock)
-						// // when block was previously rec and uploaded and we have a url to show preview
-						// if (clickedBlock && clickedBlock.objectUrl) {
-						// 	setState('preview')
-						// } else {
-						// 	// when the clicked block is not yet recorded.
-						// 	setState('resumed')
-						// }
+						// if continuous recording is enabled, disable mini-timeline onclick
+						if (
+							(continuousRecording && state === 'preview') ||
+							state === 'recording' ||
+							(recordedBlocks?.get(dataConfig[index].id)?.includes('blob') &&
+								state === 'preview')
+						) {
+							return
+						}
+            updateActiveObjectIndex(index)
+						// checking if block is recorded or not
+						if (recordedBlocks?.get(dataConfig[index].id)) {
+							updateState('preview')
+						} else {
+							updateState('resumed')
+						}
+
 					}}
 				>
-					{/* {localRecordedBlocks
-						?.find((b: RecordedBlocksFragment) => b.id === block.id)
-						?.objectUrl?.includes('.webm') && (
+					{recordedBlocks?.get(dataConfig[index].id)?.includes('.webm') && (
 						<div className='absolute top-0 right-0 rounded-tr-sm rounded-bl-sm bg-incredible-green-600'>
 							<IoCheckmarkOutline className='m-px text-gray-200' size={8} />
 						</div>
-					)} */}
+					)}
 					<span>
 						{getBlockTitle(block).substring(0, 40) +
 							(getBlockTitle(block).length > 40 ? '...' : '')}
