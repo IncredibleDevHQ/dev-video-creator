27925e20ee4ffb9a4e4c9b45021b9ec21653f061
Nithin Kumar B <nithinkumar5555@outlook.com>
2022-07-15 23:22:59 +0530

    fix(webfront): fix website favicons and fonts

--- apps/webfront/src/pages/_document.tsx
+++ apps/webfront/src/pages/_document.tsx
@@ -6,28 +6,28 @@ const Document = () => (
 			{/* Preload Gilroy and Inter Fonts */}
 			<link
 				rel='preload'
-				href='/assets/fonts/Gilroy/gilroy-extrabold-latin.woff2'
+				href='/fonts/Gilroy/gilroy-extrabold-latin.woff2'
 				as='font'
 				type='font/woff2'
 				crossOrigin='anonymous'
 			/>
 			<link
 				rel='preload'
-				href='/assets/fonts/Gilroy/gilroy-bold-latin.woff2'
+				href='/fonts/Gilroy/gilroy-bold-latin.woff2'
 				as='font'
 				type='font/woff2'
 				crossOrigin='anonymous'
 			/>
 			<link
 				rel='preload'
-				href='/assets/fonts/Gilroy/gilroy-semibold-latin.woff2'
+				href='/fonts/Gilroy/gilroy-semibold-latin.woff2'
 				as='font'
 				type='font/woff2'
 				crossOrigin='anonymous'
 			/>
 			<link
 				rel='preload'
-				href='/assets/fonts/Inter/inter-var-latin.woff2'
+				href='/fonts/Inter/inter-var-latin.woff2'
 				as='font'
 				type='font/woff2'
 				crossOrigin='anonymous'
@@ -37,31 +37,31 @@ const Document = () => (
 			<link
 				rel='apple-touch-icon'
 				sizes='180x180'
-				href='/assets/favicons/apple-touch-icon.png?v=005'
+				href='/favicons/apple-touch-icon.png?v=005'
 			/>
 			<link
 				rel='icon'
 				type='image/png'
 				sizes='32x32'
-				href='/assets/favicons/favicon-32x32.png?v=005'
+				href='/favicons/favicon-32x32.png?v=005'
 			/>
 			<link
 				rel='icon'
 				type='image/png'
 				sizes='16x16'
-				href='/assets/favicons/favicon-16x16.png?v=005'
+				href='/favicons/favicon-16x16.png?v=005'
 			/>
-			<link rel='manifest' href='/assets/favicons/site.webmanifest?v=005' />
+			<link rel='manifest' href='/favicons/site.webmanifest?v=005' />
 			<link
 				rel='mask-icon'
-				href='/assets/favicons/safari-pinned-tab.svg?v=005'
+				href='/favicons/safari-pinned-tab.svg?v=005'
 				color='#16a34a'
 			/>
-			<link rel='icon' href='/assets/favicons/favicon.ico?v=005' />
+			<link rel='icon' href='/favicons/favicon.ico?v=005' />
 			<meta name='msapplication-TileColor' content='#ffffff' />
 			<meta
 				name='msapplication-config'
-				content='/assets/favicons/browserconfig.xml?v=005'
+				content='/favicons/browserconfig.xml?v=005'
 			/>
 			<meta name='theme-color' content='#ffffff' />
 		</Head>
