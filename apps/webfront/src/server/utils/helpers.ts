8f21364373e5a550de58ed412e4eeae7395a30d0
Pythonista7 <ashwin.mirskar@gmail.com>
2022-07-18 19:47:06 +0530

    refactor(webfront/trpc): move enums to seperate file

--- apps/webfront/src/server/utils/helpers.ts
+++ apps/webfront/src/server/utils/helpers.ts
@@ -81,56 +81,6 @@ export interface MuxMetaPassThrough {
 	contentId: string
 }
 
-export enum FragmentTypeEnum {
-	Landscape = 'Landscape',
-	Portrait = 'Portrait',
-	Presentation = 'Presentation',
-}
-
-export enum FlickScopeEnum {
-	Public = 'Public',
-	Private = 'Private',
-}
-
-export enum InvitationStatusEnum {
-	Pending = 'Pending',
-	Accepted = 'Accepted',
-	Declined = 'Declined',
-	Email = 'Email',
-}
-
-export enum NotificationTypeEnum {
-	/** To communicate something async failed.  */
-	Error = 'Error',
-	/** Indicates some platform event , such as a flick is publish, blog is now live etc. */
-	Event = 'Event',
-	/** Indicates that the notification is either an invite or request to collaborate */
-	Invitation = 'Invitation',
-	/** Request */
-	Request = 'Request',
-}
-
-export enum NotificationMetaTypeEnum {
-	Flick = 'Flick',
-	Follow = 'Follow',
-	/** the meta will contains series details */
-	Series = 'Series',
-	/** the meta will contains user/profile details */
-	User = 'User',
-}
-
-export enum ParticipantRoleEnum {
-	Host = 'Host',
-	Assistant = 'Assistant',
-	Viewer = 'Viewer',
-}
-
-export enum ContentTypeEnum {
-	Blog = 'Blog',
-	VerticalVideo = 'VerticalVideo',
-	Video = 'Video',
-}
-
 export const initFirebaseAdmin = () => {
 	if (!admin.apps.length && serverEnvs.FIREBASE_SERVICE_CONFIG) {
 		return admin.initializeApp({
