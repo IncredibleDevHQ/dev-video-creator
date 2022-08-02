051a237f714d79a99ad456bd032650738473a93e
Pythonista7 <ashwin.mirskar@gmail.com>
2022-08-02 15:51:18 +0530

    feat(prisma): add seed themes and transitions

---
+++ packages/prisma/data/transitions.ts
@@ -0,0 +1,66 @@
+export const transitions = [
+	{
+		config: {
+			thumbnail: 'transitions/DipToWhite/DipToWhite.png',
+			gif: 'transitions/DipToWhite/DipToWhite.gif',
+			displayName: 'DipToWhite',
+		},
+		name: 'DipToWhite',
+	},
+	{
+		config: {
+			thumbnail: 'transitions/CloseSides/CloseSides.png',
+			gif: 'transitions/CloseSides/CloseSides.gif',
+			displayName: 'CloseSides',
+		},
+		name: 'CloseSides',
+	},
+	{
+		config: {
+			thumbnail: 'transitions/Circles/Circles.png',
+			gif: 'transitions/Circles/Circles.gif',
+			displayName: 'Circles',
+		},
+		name: 'Circles',
+	},
+	{
+		config: {
+			thumbnail: 'transitions/Wipe/Wipe.png',
+			gif: 'transitions/Wipe/Wipe.gif',
+			displayName: 'Wipe',
+		},
+		name: 'Wipe',
+	},
+	{
+		config: {
+			thumbnail: 'transitions/Triangles/Triangles.png',
+			gif: 'transitions/Triangles/Triangles.gif',
+			displayName: 'Triangles',
+		},
+		name: 'Triangles',
+	},
+	{
+		config: {
+			thumbnail: 'transitions/Rectangles/Rectangles.png',
+			gif: 'transitions/Rectangles/Rectangles.gif',
+			displayName: 'Rectangles',
+		},
+		name: 'Rectangles',
+	},
+	{
+		config: {
+			thumbnail: 'transitions/Line/Line.png',
+			gif: 'transitions/Line/Line.gif',
+			displayName: 'Line',
+		},
+		name: 'Line',
+	},
+	{
+		config: {
+			thumbnail: 'transitions/DipToBlack/DipToBlack.png',
+			gif: 'transitions/DipToBlack/DipToBlack.gif',
+			displayName: 'Dip to black',
+		},
+		name: 'DipToBlack',
+	},
+]
